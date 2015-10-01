c3eef7c7a3431f263e90b553049f0d1861d7056e
popovsn777 <popovsn777@gmail.com>
2015-10-01 22:42:08 +0300

    добавил методы

--- add_group_unittest.py
+++ add_group_unittest.py
@@ -10,7 +10,7 @@ def is_alert_present(wd):
     except:
         return False
 
-class (unittest.TestCase):
+class TestAddGroup (unittest.TestCase):
     def setUp(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
@@ -18,16 +18,21 @@ class (unittest.TestCase):
     def test_(self):
         success = True
         wd = self.wd
-        wd.get("http://localhost/addressbook/addressbook/")
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_css_selector("input[type=\"submit\"]").click()
-        wd.find_element_by_link_text("groups").click()
+        self.open_home_page(wd)
+        self.login(wd)
+        self.open_group_page(wd)
+        self.create_group(wd)
+        self.return_to_group(wd)
+        self.logout(wd)
+        self.assertTrue(success)
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def return_to_group(self, wd):
+        wd.find_element_by_link_text("group page").click()
+
+    def create_group(self, wd):
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
@@ -40,10 +45,23 @@ class (unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys("group_footer5")
         wd.find_element_by_css_selector("#content > form").click()
         wd.find_element_by_name("submit").click()
-        wd.find_element_by_link_text("group page").click()
-        wd.find_element_by_link_text("Logout").click()
-        self.assertTrue(success)
-    
+
+    def open_group_page(self, wd):
+        wd.find_element_by_link_text("groups").click()
+
+    def login(self, wd):
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_css_selector("input[type=\"submit\"]").click()
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/addressbook/")
+
     def tearDown(self):
         self.wd.quit()
 
