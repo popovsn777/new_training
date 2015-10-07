70e0b7de8d87facb4db9052662e09156a111fd53
popovsn777 <popovsn777@gmail.com>
2015-10-07 12:52:44 +0300

    добавил во все методы ссылдку wd = wd.self
    добавил во все методы ссылдку wd = wd.self
    
    и удалил из методов параметр wd , так как показано в лекции
    2_2_fixture.avi

--- add_group_unittest.py
+++ add_group_unittest.py
@@ -20,30 +20,28 @@ class TestAddGroup (unittest.TestCase):
     def test_(self):
         success = True
         wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.open_group_page(wd)
-        self.create_group(wd, Group(name="group_name5", header="group_header5", footer="group_footer5"))
-        self.return_to_group(wd)
-        self.logout(wd)
+        self.login( username="admin", password="secret")
+        self.create_group( Group(name="group_name5", header="group_header5", footer="group_footer5"))
+        self.logout()
 
     def test_add_empty_group(self):
         success = True
         wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.open_group_page(wd)
-        self.create_group(wd, Group(name="", header="", footer=""))
-        self.return_to_group(wd)
-        self.logout(wd)
+        self.login( username="admin", password="secret")
+        self.create_group( Group(name="", header="", footer=""))
+        self.logout()
 
-    def logout(self, wd):
+    def logout(self):
+        wd = self.wd
         wd.find_element_by_link_text("Logout").click()
 
-    def return_to_group(self, wd):
+    def return_to_group(self):
+        wd = self.wd
         wd.find_element_by_link_text("group page").click()
 
-    def create_group(self, wd, group):
+    def create_group(self, group):
+        wd = self.wd
+        self.open_group_page()
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
@@ -56,11 +54,14 @@ class TestAddGroup (unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         wd.find_element_by_css_selector("#content > form").click()
         wd.find_element_by_name("submit").click()
-
-    def open_group_page(self, wd):
+        self.return_to_group()
+    def open_group_page(self):
+        wd = self.wd
         wd.find_element_by_link_text("groups").click()
 
-    def login(self, wd, username, password):
+    def login(self, username, password):
+        wd = self.wd
+        self.open_home_page()
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
@@ -70,7 +71,8 @@ class TestAddGroup (unittest.TestCase):
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_css_selector("input[type=\"submit\"]").click()
 
-    def open_home_page(self, wd):
+    def open_home_page(self):
+        wd = self.wd
         wd.get("http://localhost/addressbook/addressbook/")
 
     def tearDown(self):
