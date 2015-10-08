ac71542777c7fbf5fdab95fcafa3e5b028fdc31a
popovsn777 <popovsn777@gmail.com>
2015-10-08 14:36:09 +0300

    Выдеден класс для управления сессиями

--- test/add_group_unittest.py
+++ test/add_group_unittest.py
@@ -11,11 +11,11 @@ def app(request):
 
 
 def test_(app):
-    app.login( username="admin", password="secret")
+    app.session.login( username="admin", password="secret")
     app.create_group( Group(name="group_name5", header="group_header5", footer="group_footer5"))
-    app.logout()
+    app.session.logout()
 
 def test_add_empty_group(app):
-    app.login( username="admin", password="secret")
+    app.session.login( username="admin", password="secret")
     app.create_group( Group(name="", header="", footer=""))
-    app.logout()
+    app.session.logout()
