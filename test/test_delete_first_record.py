0b4a4f5f70a63f96cc7850b54ab5a7dce1a387be
popovsn777 <popovsn777@gmail.com>
2015-10-30 17:49:59 +0300

    update1

--- test/test_delete_first_record.py
+++ test/test_delete_first_record.py
@@ -5,5 +5,16 @@ from model.member import Member
 def test_delete_first_record(app):
 
     app.new_member.delete_first_record()
+
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally: self.accept_next_alert = True
 ###    self.return_to_home_page(wd)
 
