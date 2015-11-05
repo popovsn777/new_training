2cbabbb2797f71c48e380082a1883f16826b35d4
popovsn777 <popovsn777@gmail.com>
2015-11-05 06:53:05 +0300

    добаил загрузку DB

---
+++ check_db_connection.py
@@ -0,0 +1,15 @@
+__author__ = 'Sergey'
+import mysql.connector
+
+connection = mysql.connector.connect(host="127.0.0.1", database="addressbook", user="root", password="")
+
+try:
+    cursor = connection.cursor()
+    cursor.execute("select * from group_list")
+    for row in cursor.fetchall():
+        print(row)
+
+finally:
+    connection.close()
+
+
